142b816b82d9c18fe417062c260b7f0e03ee45de
ralphvincent905 <ralph_sampaga@yahoo.com>
2024-12-13 14:24:37 -0500

    Part 2: Adding Inheritance

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Part 1: Setting Up Classes
 class ProductProperties {
     constructor(name, price, quantity) {
       this.name = name;
@@ -11,5 +12,17 @@ class ProductProperties {
   
     toString() {
       return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
+  }
+
+// Part 2: Adding Inheritance
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+      super(name, price, quantity);
+      this.expirationDate = expirationDate;
+    }
+  
+    toString() {
+      return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
     }
   }
